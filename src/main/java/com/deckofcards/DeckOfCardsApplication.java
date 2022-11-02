bbe5fe545f0df95d3bb15d3405a4ee34300a41ab
Gabriel Lucas <gabriellcontatoprofissional@gmail.com>
2022-11-02 14:03:29 -0300

    initial project

---
+++ src/main/java/com/deckofcards/DeckOfCardsApplication.java
@@ -0,0 +1,13 @@
+package com.deckofcards;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DeckOfCardsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DeckOfCardsApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/deckofcards/DeckOfCardsApplicationTests.java
@@ -0,0 +1,13 @@
+package com.deckofcards;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DeckOfCardsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
